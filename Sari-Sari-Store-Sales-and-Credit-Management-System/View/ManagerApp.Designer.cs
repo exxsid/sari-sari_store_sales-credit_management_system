c23dfd08a287a939c36d71dbde66f9c16da9a3f4
Exxsid <cortezleoanthony5@gmail.com>
2023-01-07 18:11:38 +0800

    add side navigation bar

---
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/ManagerApp.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View
+{
+    partial class ManagerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sideNavBar = new System.Windows.Forms.Panel();
+            this.analyticsSubMenu = new System.Windows.Forms.Panel();
+            this.creditAnalyticsButton = new FontAwesome.Sharp.IconButton();
+            this.salesAnalyticsButton = new FontAwesome.Sharp.IconButton();
+            this.analyticsButton = new FontAwesome.Sharp.IconButton();
+            this.usersButton = new FontAwesome.Sharp.IconButton();
+            this.productsButton = new FontAwesome.Sharp.IconButton();
+            this.saleButton = new FontAwesome.Sharp.IconButton();
+            this.homeButton = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.brandName = new System.Windows.Forms.Label();
+            this.sideNavBar.SuspendLayout();
+            this.analyticsSubMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideNavBar
+            // 
+            this.sideNavBar.AutoScroll = true;
+            this.sideNavBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(4)))), ((int)(((byte)(94)))));
+            this.sideNavBar.Controls.Add(this.analyticsSubMenu);
+            this.sideNavBar.Controls.Add(this.analyticsButton);
+            this.sideNavBar.Controls.Add(this.usersButton);
+            this.sideNavBar.Controls.Add(this.productsButton);
+            this.sideNavBar.Controls.Add(this.saleButton);
+            this.sideNavBar.Controls.Add(this.homeButton);
+            this.sideNavBar.Controls.Add(this.panel2);
+            this.sideNavBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideNavBar.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sideNavBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.sideNavBar.Location = new System.Drawing.Point(0, 0);
+            this.sideNavBar.Name = "sideNavBar";
+            this.sideNavBar.Size = new System.Drawing.Size(194, 476);
+            this.sideNavBar.TabIndex = 0;
+            // 
+            // analyticsSubMenu
+            // 
+            this.analyticsSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(199)))));
+            this.analyticsSubMenu.Controls.Add(this.creditAnalyticsButton);
+            this.analyticsSubMenu.Controls.Add(this.salesAnalyticsButton);
+            this.analyticsSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.analyticsSubMenu.Location = new System.Drawing.Point(0, 384);
+            this.analyticsSubMenu.Name = "analyticsSubMenu";
+            this.analyticsSubMenu.Size = new System.Drawing.Size(194, 90);
+            this.analyticsSubMenu.TabIndex = 8;
+            // 
+            // creditAnalyticsButton
+            // 
+            this.creditAnalyticsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.creditAnalyticsButton.FlatAppearance.BorderSize = 0;
+            this.creditAnalyticsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.creditAnalyticsButton.IconChar = FontAwesome.Sharp.IconChar.CreditCardAlt;
+            this.creditAnalyticsButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.creditAnalyticsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.creditAnalyticsButton.IconSize = 25;
+            this.creditAnalyticsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.creditAnalyticsButton.Location = new System.Drawing.Point(0, 38);
+            this.creditAnalyticsButton.Name = "creditAnalyticsButton";
+            this.creditAnalyticsButton.Padding = new System.Windows.Forms.Padding(25, 0, 10, 0);
+            this.creditAnalyticsButton.Size = new System.Drawing.Size(194, 38);
+            this.creditAnalyticsButton.TabIndex = 9;
+            this.creditAnalyticsButton.Text = "Credits Analytics";
+            this.creditAnalyticsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.creditAnalyticsButton.UseVisualStyleBackColor = true;
+            // 
+            // salesAnalyticsButton
+            // 
+            this.salesAnalyticsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.salesAnalyticsButton.FlatAppearance.BorderSize = 0;
+            this.salesAnalyticsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.salesAnalyticsButton.IconChar = FontAwesome.Sharp.IconChar.CartShopping;
+            this.salesAnalyticsButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.salesAnalyticsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.salesAnalyticsButton.IconSize = 25;
+            this.salesAnalyticsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.salesAnalyticsButton.Location = new System.Drawing.Point(0, 0);
+            this.salesAnalyticsButton.Name = "salesAnalyticsButton";
+            this.salesAnalyticsButton.Padding = new System.Windows.Forms.Padding(25, 0, 10, 0);
+            this.salesAnalyticsButton.Size = new System.Drawing.Size(194, 38);
+            this.salesAnalyticsButton.TabIndex = 8;
+            this.salesAnalyticsButton.Text = "Sales Analytics";
+            this.salesAnalyticsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.salesAnalyticsButton.UseVisualStyleBackColor = true;
+            // 
+            // analyticsButton
+            // 
+            this.analyticsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.analyticsButton.FlatAppearance.BorderSize = 0;
+            this.analyticsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.analyticsButton.IconChar = FontAwesome.Sharp.IconChar.BarChart;
+            this.analyticsButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.analyticsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.analyticsButton.IconSize = 25;
+            this.analyticsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.analyticsButton.Location = new System.Drawing.Point(0, 334);
+            this.analyticsButton.Name = "analyticsButton";
+            this.analyticsButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.analyticsButton.Size = new System.Drawing.Size(194, 50);
+            this.analyticsButton.TabIndex = 7;
+            this.analyticsButton.Text = "Analytics";
+            this.analyticsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.analyticsButton.UseVisualStyleBackColor = true;
+            this.analyticsButton.Click += new System.EventHandler(this.analyticsButton_Click);
+            // 
+            // usersButton
+            // 
+            this.usersButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.usersButton.FlatAppearance.BorderSize = 0;
+            this.usersButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.usersButton.IconChar = FontAwesome.Sharp.IconChar.Users;
+            this.usersButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.usersButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.usersButton.IconSize = 25;
+            this.usersButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.usersButton.Location = new System.Drawing.Point(0, 284);
+            this.usersButton.Name = "usersButton";
+            this.usersButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.usersButton.Size = new System.Drawing.Size(194, 50);
+            this.usersButton.TabIndex = 6;
+            this.usersButton.Text = "Users";
+            this.usersButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.usersButton.UseVisualStyleBackColor = true;
+            this.usersButton.Click += new System.EventHandler(this.usersButton_Click);
+            // 
+            // productsButton
+            // 
+            this.productsButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.productsButton.FlatAppearance.BorderSize = 0;
+            this.productsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.productsButton.IconChar = FontAwesome.Sharp.IconChar.Tag;
+            this.productsButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.productsButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.productsButton.IconSize = 25;
+            this.productsButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.productsButton.Location = new System.Drawing.Point(0, 234);
+            this.productsButton.Name = "productsButton";
+            this.productsButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.productsButton.Size = new System.Drawing.Size(194, 50);
+            this.productsButton.TabIndex = 5;
+            this.productsButton.Text = "Products";
+            this.productsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.productsButton.UseVisualStyleBackColor = true;
+            this.productsButton.Click += new System.EventHandler(this.productsButton_Click);
+            // 
+            // saleButton
+            // 
+            this.saleButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.saleButton.FlatAppearance.BorderSize = 0;
+            this.saleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saleButton.IconChar = FontAwesome.Sharp.IconChar.CartShopping;
+            this.saleButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.saleButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.saleButton.IconSize = 25;
+            this.saleButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.saleButton.Location = new System.Drawing.Point(0, 184);
+            this.saleButton.Name = "saleButton";
+            this.saleButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.saleButton.Size = new System.Drawing.Size(194, 50);
+            this.saleButton.TabIndex = 4;
+            this.saleButton.Text = "Sale";
+            this.saleButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.saleButton.UseVisualStyleBackColor = true;
+            this.saleButton.Click += new System.EventHandler(this.saleButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.homeButton.FlatAppearance.BorderSize = 0;
+            this.homeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.homeButton.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.homeButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.homeButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.homeButton.IconSize = 25;
+            this.homeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.homeButton.Location = new System.Drawing.Point(0, 134);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.homeButton.Size = new System.Drawing.Size(194, 50);
+            this.homeButton.TabIndex = 3;
+            this.homeButton.Text = "Home";
+            this.homeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.brandName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(194, 134);
+            this.panel2.TabIndex = 2;
+            // 
+            // brandName
+            // 
+            this.brandName.AutoSize = true;
+            this.brandName.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brandName.Location = new System.Drawing.Point(48, 23);
+            this.brandName.Name = "brandName";
+            this.brandName.Size = new System.Drawing.Size(89, 25);
+            this.brandName.TabIndex = 0;
+            this.brandName.Text = "Mini-Mart";
+            // 
+            // ManagerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 476);
+            this.Controls.Add(this.sideNavBar);
+            this.Name = "ManagerApp";
+            this.Text = "ManagerApp";
+            this.Load += new System.EventHandler(this.ManagerApp_Load);
+            this.sideNavBar.ResumeLayout(false);
+            this.analyticsSubMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel sideNavBar;
+        private System.Windows.Forms.Label brandName;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconButton homeButton;
+        private FontAwesome.Sharp.IconButton productsButton;
+        private FontAwesome.Sharp.IconButton saleButton;
+        private FontAwesome.Sharp.IconButton usersButton;
+        private FontAwesome.Sharp.IconButton analyticsButton;
+        private System.Windows.Forms.Panel analyticsSubMenu;
+        private FontAwesome.Sharp.IconButton creditAnalyticsButton;
+        private FontAwesome.Sharp.IconButton salesAnalyticsButton;
+    }
+}
---
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/ManagerApp.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View
+{
+    public partial class ManagerApp : Form
+    {
+        private System.Drawing.Color NONACTIVE_BUTTON_BACK_COLOR = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(4)))), ((int)(((byte)(94)))));
+        private System.Drawing.Color ACTIVE_BUTTON_BACK_COLOR = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(62)))), ((int)(((byte)(138)))));
+
+        public ManagerApp()
+        {
+            InitializeComponent();
+        }
+
+        private void ManagerApp_Load(object sender, EventArgs e)
+        {
+            // hide the analytics submenu in load time
+            this.analyticsSubMenu.Visible = false;
+        }
+
+        #region homeButton
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            // set the back color
+            setToActiveButtonBackColor(this.homeButton);
+        }
+        #endregion
+
+        #region saleButton
+        private void saleButton_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            // set the back color
+            setToActiveButtonBackColor(this.saleButton);
+        }
+        #endregion
+
+        #region productsButton
+        private void productsButton_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            // set the back color
+            setToActiveButtonBackColor(this.productsButton);
+        }
+        #endregion
+
+        #region usersButton
+        private void usersButton_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+            // set the back color
+            setToActiveButtonBackColor(this.usersButton);
+        }
+        #endregion
+
+        #region analyticsButton
+        private void analyticsButton_Click(object sender, EventArgs e)
+        {
+            showSubMenu(this.analyticsSubMenu);
+            // set the back color
+            setToActiveButtonBackColor(this.analyticsButton);
+
+        }
+        #endregion
+
+        private void hideSubMenu()
+        {
+            // hide the analytics submenu when other button is clicked
+            if (this.analyticsSubMenu.Visible)
+            {
+                this.analyticsSubMenu.Visible = false;
+            }
+        }
+
+
+        private void showSubMenu(Panel subMenu)
+        {
+            // // hide the analytics submenu when the specified button is clicked
+            if (!subMenu.Visible)
+            {
+                hideSubMenu();// closed the submenu that is shown
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private void setToNonActiveButtonBackColor()
+        {
+            if (this.homeButton.BackColor == ACTIVE_BUTTON_BACK_COLOR)
+            {
+                this.homeButton.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+
+            if (this.saleButton.BackColor == ACTIVE_BUTTON_BACK_COLOR)
+            {
+                this.saleButton.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+
+            if (this.productsButton.BackColor == ACTIVE_BUTTON_BACK_COLOR)
+            {
+                this.productsButton.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+
+            if (this.usersButton.BackColor == ACTIVE_BUTTON_BACK_COLOR)
+            {
+                this.usersButton.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+
+            if (this.analyticsButton.BackColor == ACTIVE_BUTTON_BACK_COLOR)
+            {
+                this.analyticsButton.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+        }
+
+        private void setToActiveButtonBackColor(Button button)
+        {
+            // when the button is not active the 
+            if (button.BackColor == NONACTIVE_BUTTON_BACK_COLOR)
+            {
+                setToNonActiveButtonBackColor();
+                button.BackColor = ACTIVE_BUTTON_BACK_COLOR;
+            }
+            else
+            {
+                button.BackColor = NONACTIVE_BUTTON_BACK_COLOR;
+            }
+        }
+
+       
+    }
+}
