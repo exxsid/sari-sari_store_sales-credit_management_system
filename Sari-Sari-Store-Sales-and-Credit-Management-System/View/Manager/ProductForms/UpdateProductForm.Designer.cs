28643525e74c949acc31313f56b491a57294982d
exxsid <cortezleoanthony5@gmail.com>
2023-01-12 23:33:52 +0800

    add product and user forms for adding and updating

---
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ProductForms/UpdateProductForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View.Manager.ProductForms
+{
+    partial class UpdateProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.boughtPriceLabel = new System.Windows.Forms.Label();
+            this.priceTextbox = new System.Windows.Forms.TextBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.quantityTextbox = new System.Windows.Forms.TextBox();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.productNameTextbox = new System.Windows.Forms.TextBox();
+            this.productNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.SystemColors.Control;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveButton.Location = new System.Drawing.Point(99, 290);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 31);
+            this.saveButton.TabIndex = 19;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.Red;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cancelButton.Location = new System.Drawing.Point(318, 290);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 31);
+            this.cancelButton.TabIndex = 18;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // boughtPriceLabel
+            // 
+            this.boughtPriceLabel.AutoSize = true;
+            this.boughtPriceLabel.Location = new System.Drawing.Point(96, 225);
+            this.boughtPriceLabel.Name = "boughtPriceLabel";
+            this.boughtPriceLabel.Size = new System.Drawing.Size(83, 16);
+            this.boughtPriceLabel.TabIndex = 16;
+            this.boughtPriceLabel.Text = "Bought Price";
+            // 
+            // priceTextbox
+            // 
+            this.priceTextbox.Location = new System.Drawing.Point(99, 174);
+            this.priceTextbox.Name = "priceTextbox";
+            this.priceTextbox.Size = new System.Drawing.Size(294, 22);
+            this.priceTextbox.TabIndex = 15;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(96, 155);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(38, 16);
+            this.priceLabel.TabIndex = 14;
+            this.priceLabel.Text = "Price";
+            // 
+            // quantityTextbox
+            // 
+            this.quantityTextbox.Location = new System.Drawing.Point(99, 104);
+            this.quantityTextbox.Name = "quantityTextbox";
+            this.quantityTextbox.Size = new System.Drawing.Size(294, 22);
+            this.quantityTextbox.TabIndex = 13;
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(96, 85);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(55, 16);
+            this.quantityLabel.TabIndex = 12;
+            this.quantityLabel.Text = "Quantity";
+            // 
+            // productNameTextbox
+            // 
+            this.productNameTextbox.Location = new System.Drawing.Point(99, 34);
+            this.productNameTextbox.Name = "productNameTextbox";
+            this.productNameTextbox.Size = new System.Drawing.Size(294, 22);
+            this.productNameTextbox.TabIndex = 11;
+            // 
+            // productNameLabel
+            // 
+            this.productNameLabel.AutoSize = true;
+            this.productNameLabel.Location = new System.Drawing.Point(96, 15);
+            this.productNameLabel.Name = "productNameLabel";
+            this.productNameLabel.Size = new System.Drawing.Size(93, 16);
+            this.productNameLabel.TabIndex = 10;
+            this.productNameLabel.Text = "Product Name";
+            // 
+            // UpdateProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 337);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.boughtPriceLabel);
+            this.Controls.Add(this.priceTextbox);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.quantityTextbox);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.productNameTextbox);
+            this.Controls.Add(this.productNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "UpdateProductForm";
+            this.Text = "UpdateProductForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label boughtPriceLabel;
+        private System.Windows.Forms.TextBox priceTextbox;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox quantityTextbox;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.TextBox productNameTextbox;
+        private System.Windows.Forms.Label productNameLabel;
+    }
+}
---
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/UserForms/AddUserForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View.Manager.UserForms
+{
+    partial class AddUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.typeCombobox = new System.Windows.Forms.ComboBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(66, 29);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(44, 16);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(69, 48);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(273, 22);
+            this.nameTextbox.TabIndex = 1;
+            // 
+            // typeCombobox
+            // 
+            this.typeCombobox.Enabled = false;
+            this.typeCombobox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.typeCombobox.FormattingEnabled = true;
+            this.typeCombobox.Items.AddRange(new object[] {
+            "Cashier",
+            "Manager"});
+            this.typeCombobox.Location = new System.Drawing.Point(69, 180);
+            this.typeCombobox.Name = "typeCombobox";
+            this.typeCombobox.Size = new System.Drawing.Size(273, 24);
+            this.typeCombobox.TabIndex = 2;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(69, 114);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(273, 22);
+            this.passwordTextbox.TabIndex = 4;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(66, 95);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(67, 16);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(66, 161);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(104, 16);
+            this.typeLabel.TabIndex = 5;
+            this.typeLabel.Text = "Employee Type";
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.SystemColors.Control;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveButton.Location = new System.Drawing.Point(69, 234);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 31);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.Red;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cancelButton.Location = new System.Drawing.Point(267, 234);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 31);
+            this.cancelButton.TabIndex = 10;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            // 
+            // AddUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 315);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.typeCombobox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.nameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddUserForm";
+            this.Text = "Add User Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.ComboBox typeCombobox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
