9bdac28529b7117ca206420c4b23f696e6c9287e
exxsid <cortezleoanthony5@gmail.com>
2023-01-16 20:27:30 +0800

    change FormBorderStyle for NewProductForm

--- Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ProductForms/NewProductForm.Designer.cs
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ProductForms/NewProductForm.Designer.cs
@@ -144,7 +144,7 @@
             this.Controls.Add(this.productNameTextbox);
             this.Controls.Add(this.productNameLabel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "NewProductForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
