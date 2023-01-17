74f2d071a7772982e9990e49d4347953e1216b83
exxsid <cortezleoanthony5@gmail.com>
2023-01-17 22:10:41 +0800

    sales analytics total sales

--- Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ManagerSalesAnalyticsUserControl.Designer.cs
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ManagerSalesAnalyticsUserControl.Designer.cs
@@ -29,12 +29,19 @@
         private void InitializeComponent()
         {
             this.salesAnalyticsDatagridView = new System.Windows.Forms.DataGridView();
-            this.salesChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.loadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totalSaleDisplay = new System.Windows.Forms.Label();
+            this.comparisonDisplay = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.salesAnalyticsDatagridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.salesChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // salesAnalyticsDatagridView
@@ -43,77 +50,154 @@
             this.salesAnalyticsDatagridView.AllowUserToDeleteRows = false;
             this.salesAnalyticsDatagridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.salesAnalyticsDatagridView.Dock = System.Windows.Forms.DockStyle.Right;
-            this.salesAnalyticsDatagridView.Location = new System.Drawing.Point(402, 0);
+            this.salesAnalyticsDatagridView.Location = new System.Drawing.Point(317, 0);
             this.salesAnalyticsDatagridView.Name = "salesAnalyticsDatagridView";
             this.salesAnalyticsDatagridView.ReadOnly = true;
-            this.salesAnalyticsDatagridView.Size = new System.Drawing.Size(273, 403);
+            this.salesAnalyticsDatagridView.Size = new System.Drawing.Size(358, 403);
             this.salesAnalyticsDatagridView.TabIndex = 0;
             // 
-            // salesChart
+            // dateTimePicker
             // 
-            this.salesChart.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.salesChart.Location = new System.Drawing.Point(0, 98);
-            this.salesChart.Name = "salesChart";
-            this.salesChart.Size = new System.Drawing.Size(402, 305);
-            this.salesChart.TabIndex = 1;
-            this.salesChart.Text = "chart1";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Daily",
-            "Weekly",
-            "Monthly"});
-            this.comboBox1.Location = new System.Drawing.Point(39, 53);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(39, 13);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(258, 25);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker.CustomFormat = "MMMM yyyy";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(22, 13);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(192, 25);
+            this.dateTimePicker.TabIndex = 3;
+            this.dateTimePicker.Value = new System.DateTime(2023, 1, 17, 0, 0, 0, 0);
             // 
             // loadButton
             // 
             this.loadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.loadButton.Location = new System.Drawing.Point(248, 53);
+            this.loadButton.Location = new System.Drawing.Point(220, 13);
             this.loadButton.Name = "loadButton";
-            this.loadButton.Size = new System.Drawing.Size(134, 28);
+            this.loadButton.Size = new System.Drawing.Size(75, 28);
             this.loadButton.TabIndex = 4;
             this.loadButton.Text = "Load";
             this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Total Sales:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Comparision to last month:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Top-selling products";
+            // 
+            // totalSaleDisplay
+            // 
+            this.totalSaleDisplay.AutoSize = true;
+            this.totalSaleDisplay.Location = new System.Drawing.Point(59, 89);
+            this.totalSaleDisplay.Name = "totalSaleDisplay";
+            this.totalSaleDisplay.Size = new System.Drawing.Size(53, 20);
+            this.totalSaleDisplay.TabIndex = 8;
+            this.totalSaleDisplay.Text = "P 0.00";
+            // 
+            // comparisonDisplay
+            // 
+            this.comparisonDisplay.AutoSize = true;
+            this.comparisonDisplay.Location = new System.Drawing.Point(231, 160);
+            this.comparisonDisplay.Name = "comparisonDisplay";
+            this.comparisonDisplay.Size = new System.Drawing.Size(48, 20);
+            this.comparisonDisplay.TabIndex = 9;
+            this.comparisonDisplay.Text = "100%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total Sales increased by";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(63, 231);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Product Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Sales";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             // 
             // ManagerSalesAnalyticsUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comparisonDisplay);
+            this.Controls.Add(this.totalSaleDisplay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.loadButton);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.salesChart);
+            this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.salesAnalyticsDatagridView);
             this.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ManagerSalesAnalyticsUserControl";
             this.Size = new System.Drawing.Size(675, 403);
             ((System.ComponentModel.ISupportInitialize)(this.salesAnalyticsDatagridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.salesChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView salesAnalyticsDatagridView;
-        private System.Windows.Forms.DataVisualization.Charting.Chart salesChart;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
         private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label totalSaleDisplay;
+        private System.Windows.Forms.Label comparisonDisplay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
--- Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ManagerSalesAnalyticsUserControl.cs
+++ Sari-Sari-Store-Sales-and-Credit-Management-System/View/Manager/ManagerSalesAnalyticsUserControl.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sari_Sari_Store_Sales_and_Credit_Management_System.Misc;
+using MySql.Data.MySqlClient;
 
 namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View.Manager
 {
@@ -15,6 +17,43 @@ namespace Sari_Sari_Store_Sales_and_Credit_Management_System.View.Manager
         public ManagerSalesAnalyticsUserControl()
         {
             InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = DBConnector.Connector();
+            // get the month from the datetimepicker
+            string month = dateTimePicker.Value.ToString("MM");
+            // get the year from the datetimepicker
+            string year = dateTimePicker.Value.ToString("yyyy");
+
+            #region total sales
+            // get the total sales
+            conn.Open();
+            string totalSaleQuery = "SELECT sum(price) FROM sale_details " +
+                "JOIN sales ON sale_details.sale_id = sales.id " +
+                "WHERE month(date) = @month AND year(date) = @year;";
+            var totalSaleCmd = new MySqlCommand(totalSaleQuery, conn);
+            totalSaleCmd.Parameters.AddWithValue("@month", month);
+            totalSaleCmd.Parameters.AddWithValue("@year", year);
+            var ttlSaleReader = totalSaleCmd.ExecuteReader();
+
+            // when there is no sales in the specified moth and year
+            ttlSaleReader.Read();
+            if(ttlSaleReader.IsDBNull(0))
+            {
+                conn.Close();
+                this.totalSaleDisplay.Text = "P " + 0;
+                return;
+            }
+
+            double totalSale = ttlSaleReader.GetDouble(0);
+            this.totalSaleDisplay.Text = "P " + totalSale.ToString();
+
+            conn.Close();
+            #endregion
+
+            
         }
     }
 }
